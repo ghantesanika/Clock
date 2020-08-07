0bf8fec7f2d0a0024388f5158f2b072a18529be2
ghantesanika <sanikaghante@gamil.com>
2020-08-07 15:42:47 +0530

    added setRotation function

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function setClock() {
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+  }
+  
+setClock()
